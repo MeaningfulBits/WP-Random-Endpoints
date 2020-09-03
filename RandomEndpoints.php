680669fc2f8a6b2f54af69c8eca7a81dae3d40f1
T. Thomas <TdotThomas@SpaceAgeMinds.com>
2020-09-03 16:54:55 -0700

    1. Added code stubs for RandomEndpoints WordPress plugin.

---
+++ RandomEndpoints.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   Plugin Name: Random Endpoints
+   Plugin URI: https//www.SpaceAgeMinds.com
+   description: >-
+  a plugin to add random endpoints.
+   Version: 0.1
+   Author: T. Thomas
+   Author URI: https://www.SpaceAgeMinds.com
+   License: GPL2
+   */
+?>
