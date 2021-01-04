85af46859d68b125914c3043ce9990bf07fe9ec6
T. Thomas <TdotThomas@SpaceAgeMinds.com>
2021-01-03 20:41:04 -0800

    1. Added the base code for the random enpoint plugin.
    This plugin helps send users to a random page on the website. Sending users to a random page or random set of pages will help shape traffic.
    
    This plugin should be as system agnogstic as possible. While this inital plugin is targeted at wordpress this plugin might be used for another system in the future.

--- RandomEndpoints.php
+++ RandomEndpoints.php
@@ -1,12 +1,51 @@
 <?php
-   /*
-   Plugin Name: Random Endpoints
-   Plugin URI: https//www.SpaceAgeMinds.com
-   description: >-
-  a plugin to add random endpoints.
-   Version: 0.1
-   Author: T. Thomas
-   Author URI: https://www.SpaceAgeMinds.com
-   License: GPL2
-   */
+/*
+Plugin Name: Random Endpoints
+Plugin URI: https//www.SpaceAgeMinds.com
+Description: A plugin to add random endpoints.
+Version: 0.1
+Author: T. Thomas
+Author URI: https://www.SpaceAgeMinds.com
+License: GPL2
+*/
+
+//Start Custom Code for a Random Endpoints
+function random_endpoint() {
+	    add_rewrite_endpoint( 'random', EP_ROOT );
+}
+add_action( 'init', 'random_endpoint' );
+
+function random_redirect() {
+// If we have accessed our /random/ endpoints.
+$post_type = get_query_var( 'random' );
+if ( $post_type == '' ) {
+	$post_type = [ 'post', 'page', 'product' ];
+}
+$query_string = "";
+$arr = explode("?",$_SERVER['REQUEST_URI']);
+if (count($arr) == 2){
+	$query_string = "?".end($arr);
+}
+if ( get_query_var( 'random', false ) !== false ) {
+	// Get a random post.
+	$random_post = get_posts( [
+            'numberposts' => 1,
+            'post_type'   => $post_type,
+            'orderby'     => 'rand',
+        ] );
+        // If we found one.
+        if ( ! empty( $random_post ) ) {
+            // Get its URL.
+            $url = esc_url_raw( get_the_permalink( $random_post[0] ) );
+            // Escape it.
+            $url = esc_url_raw($url.$query_string);
+            // Redirect to it.
+            wp_safe_redirect( $url, 302 );
+            exit;
+        }
+    }
+}
+add_action( 'template_redirect', 'random_redirect' );
+//End Custom Code for a Random Endpoints
+
 ?>
